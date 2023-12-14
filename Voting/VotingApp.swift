d9cf37a069ec125e4e185f3b07a4076cd0abef74
Tsuri Yuma <yuma33t@gmail.com>
2023-12-15 01:43:31 +0900

    Initial Commit

---
+++ Voting/VotingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VotingApp.swift
+//  Voting
+//
+//  Created by Yuma on 2023/12/15.
+//
+
+import SwiftUI
+
+@main
+struct VotingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
